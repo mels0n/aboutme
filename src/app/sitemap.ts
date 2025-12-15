eb36fde298229617a30fa77cd8fca4aabf93127a
Chris <chris.m.melson@gmail.com>
2025-12-15 16:27:37 -0600

    fix(seo): change sitemap frequency to monthly

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,7 +5,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
         {
             url: 'https://chris.melson.us',
             lastModified: new Date(),
-            changeFrequency: 'weekly',
+            changeFrequency: 'monthly',
             priority: 1,
         },
     ];
